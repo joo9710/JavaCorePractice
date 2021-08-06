e4f87be08481871cea394af790ac24b474a2f1b8
이주형 <joo9710@gmail.com>
2021-08-06 13:40:00 +0900

    upload files

---
+++ src/kr/ac/untitled/src/kr/ac/dgd/Main.java
@@ -0,0 +1,25 @@
+package kr.ac.dgd;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //interface란 뭔가? : 서로 다른 객체의 공통된 기능을 쓸 수 있도록 한다.
+
+        List<String> arraylist = new ArrayList<String>();
+        List<String> linkedList2 = new LinkedList<String>();
+
+        // arrayList에 string 데이터를 넣는다.
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        // linkedlist에 string 데이터를 넣는다.
+        linkedList.add("1");
+        linkedList.add("2");
+        linkedList.add("3");
+
+    }
+}
